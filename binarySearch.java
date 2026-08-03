47c0c1196c546c123d7b5de335fa3aa8956799b8
Abhijith Kumar <156582037+abhijithkCS-1289@users.noreply.github.com>
2026-08-03 12:12:09 +0530

    Optimized BinarySearch code in Java

---
+++ binarySearch.java
@@ -0,0 +1,25 @@
+public class binarySearch {
+    public static int BinarySearch(int numbers[], int key) {
+        int start = 0, end = (numbers.length) - 1;
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+                
+            if(numbers[mid] == key) {
+                return mid;
+            }
+            if(numbers[mid] < key) {     
+                start = mid + 1;
+            } else  {
+                end = mid - 1;
+            }
+        }
+        return -1;
+}
+    public static void main(String[] args) {
+    int numbers[] = {2, 4, 6, 8, 10, 12};
+    int key = 10;
+
+    System.out.println("Index is located at: " + BinarySearch(numbers, key));
+    }
+}
