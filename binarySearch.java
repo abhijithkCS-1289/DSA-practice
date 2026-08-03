e1c1a01369e80a9383fde5f880148d4df0ef3e79
Abhijith Kumar <156582037+abhijithkCS-1289@users.noreply.github.com>
2026-08-03 22:11:53 +0530

    Binary search method and user input handling

--- binarySearch.java
+++ binarySearch.java
@@ -1,25 +1,35 @@
+import java.util.*;
+
 public class binarySearch {
-    public static int BinarySearch(int numbers[], int key) {
-        int start = 0, end = (numbers.length) - 1;
+    public static int BinaryS(int numlist[], int key) {
+        int start = 0, end = (numlist.length) - 1;
 
         while(start <= end) {
             int mid = (start + end) / 2;
-                
-            if(numbers[mid] == key) {
+
+            if(numlist[mid] == key) {
                 return mid;
             }
-            if(numbers[mid] < key) {     
+            if(numlist[mid] < key) { //right-side
                 start = mid + 1;
-            } else  {
+            }   else    {
                 end = mid - 1;
             }
         }
-        return -1;
-}
-    public static void main(String[] args) {
-    int numbers[] = {2, 4, 6, 8, 10, 12};
-    int key = 10;
+    return -1;
+    }
 
-    System.out.println("Index is located at: " + BinarySearch(numbers, key));
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int array_of_num[] = {12, 23, 34, 45, 56, 67, 78, 89, 90};
+
+        System.out.println("Enter the element to find: ");
+        int key = input.nextInt();
+
+        if(key == BinaryS(array_of_num, key)) {
+            System.out.println("Element is found at the Index: " + BinaryS(array_of_num, key));
+        }   else    {
+            System.out.println("Element is NOT FOUND in the list. Try again.");
+        }
     }
 }
