69708aa9b8c2f47bbca6c6cd401df826a8423c92
Abhijith Kumar <156582037+abhijithkCS-1289@users.noreply.github.com>
2026-08-13 19:23:00 +0530

    Maximum number of a subarray using Brute Force

---
+++ maxArraySum.java
@@ -0,0 +1,25 @@
+public class maxArraySum {
+    public static void ArraySum(int num[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i = 0; i < num.length; i++) {
+            int start = i;
+            for(int j = i; j < num.length; j++) {
+                int end = j;
+                currSum = 0;
+                for(int k = start; k <= end; k++) {
+                    currSum += num[k];
+                }
+            System.out.println(currSum);
+                if(maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("Max Sum is: " + maxSum);
+    }    
+    public static void main(String[] args) {
+        int num[] = {1, -2, 6, -1, 3};
+        ArraySum(num);
+    }
+}
